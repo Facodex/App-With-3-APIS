c6a619f020241b1ed7b94df3386b52363c424ebb
Facundo Benitez <facu.cr9@gmail.com>
2023-03-23 12:10:49 -0300

    interface reqresAPI ready

---
+++ src/hooks/useFetchUsers.jsx
@@ -0,0 +1,8 @@
+export const useFetchUsers = async (url, setState) => {
+    const request = await fetch(url);
+    const response = await request.json();
+    setState(response.data);
+    return response.data;
+};
+
+// export default useFetchUsers;
--- src/reqres/AccordionReqres.jsx
+++ src/reqres/AccordionReqres.jsx
@@ -1,34 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import Accordion from 'react-bootstrap/Accordion';
 import SpinnerLittle from '../components/SpinnerLittle';
-
-const fetchUsers = async (url, setState) => {
-  const request = await fetch(url);
-  const response = await request.json();
-  setState(response.data);
-  return response.data;
-};
+import { useFetchUsers } from '../hooks/useFetchUsers';
 
 function AccordionReqres() {
   const [users, setUsers] = useState(null);
   const [resources, setResources] = useState(null);
 
   useEffect(() => {
-    fetchUsers('https://reqres.in/api/users?page=2', setUsers);
-    fetchUsers('https://reqres.in/api/unknown', setResources);
+    useFetchUsers('https://reqres.in/api/users?page=2', setUsers);
+    useFetchUsers('https://reqres.in/api/products', setResources);
   }, []);
 
   return (
     <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0" onClick={fetchUsers}>
-        <Accordion.Header>Pide una lista de usuarios</Accordion.Header>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>REQUEST A LIST OF USERS</Accordion.Header>
         <Accordion.Body>
           {
             users ? users.map(user => {
               return (
-                <div className='d-flex justify-content-around'>
-                  <img src={user.avatar} className="rounded w-5 h-5" alt="Foto de perfil" />
-                  <p key={user.id}>{user.first_name} - </p>
+                <div key={user.id} className='d-flex justify-content-start align-items-center mb-2'>
+                  <img src={user.avatar} className="rounded-circle avatarReqres mx-3" alt="Foto de perfil" />
+                  <p className='fw-bold mx-2'>{user.first_name} {user.first_name}</p>
+                  <p className='text-primary'>Email: {user.email} </p>
                 </div>
               )
             })
@@ -38,27 +33,26 @@ function AccordionReqres() {
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header>REQUEST A LIST OF RESOURCES</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          {
+            resources ? resources.map(product => {
+              return (
+                <div key={product.id} className='d-flex justify-content-start align-items-center mb-2'>
+                  <ul className='mx-2'>
+                    <li>Name resource: {product.name}</li>
+                    <li>Year: {product.year}</li>
+                    <li>Pantone value: {product.pantone_value}</li>
+                  </ul>
+                  <div className="imgReqresResource" style={{backgroundColor: product.color}}>
+
+                  </div>
+                </div>
+              )
+            })
+              :
+              <SpinnerLittle />
+          }
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
